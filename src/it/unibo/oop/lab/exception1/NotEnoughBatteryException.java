143d0d713b19be41eccf7e23d89b8740030e7974
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-11-08 17:05:14 +0100

    feat: Added single constructor of the exception

--- src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
+++ src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
@@ -2,6 +2,21 @@ package it.unibo.oop.lab.exception1;
 
 public class NotEnoughBatteryException extends IllegalStateException {
 
-	
+	private final double batteryLeft;
+    private final double batteryRequired;
+
+    /**
+     * Construct new instance of the exception.
+     * 
+     * @param batteryLeft
+     *            battery level left in the robot
+     * @param batteryRequired
+     *            battery level required to do the action
+     */
+    public NotEnoughBatteryException(final double batteryLeft, final double batteryRequired) {
+        super();
+        this.batteryLeft = batteryLeft;
+        this.batteryRequired = batteryRequired;
+    }
 
 }
