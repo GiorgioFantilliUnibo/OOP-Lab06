f838d0400cd445fab8232f35426ea7d8c8f688d8
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-11-13 22:29:40 +0100

    feat: add constructor

--- src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
+++ src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
@@ -4,5 +4,10 @@ package it.unibo.oop.lab.exception2;
 public class WrongAccountHolderException extends IllegalArgumentException {
 	
 	private final String message;
-	
+
+	public WrongAccountHolderException(final String message) {
+		super();
+		this.message = message;
+	}
+
 }
