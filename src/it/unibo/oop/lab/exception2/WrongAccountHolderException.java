a7d41435922fe196a74a8607a8f54cd5273bd007
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-11-14 16:54:39 +0100

    refactor!: empty the WrongAccountHolderException class
    Message string and related implementation no longer necessary

--- src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
+++ src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
@@ -3,16 +3,4 @@ package it.unibo.oop.lab.exception2;
 @SuppressWarnings("serial")
 public class WrongAccountHolderException extends IllegalArgumentException {
 	
-	private final String message;
-
-	public WrongAccountHolderException(final String message) {
-		super();
-		this.message = message;
-	}
-
-	@Override
-	public String toString() {
-		return message == null ? "" : "WrongAccountHolderException [message=" + message + "]";
-	}
-	
 }
