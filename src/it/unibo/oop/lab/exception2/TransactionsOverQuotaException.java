5b27aeb32de5461bc0013d7f34f38bc3e34ddc48
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-11-14 17:47:22 +0100

    feat: add custom toString()

--- src/it/unibo/oop/lab/exception2/TransactionsOverQuotaException.java
+++ src/it/unibo/oop/lab/exception2/TransactionsOverQuotaException.java
@@ -3,4 +3,9 @@ package it.unibo.oop.lab.exception2;
 @SuppressWarnings("serial")
 public class TransactionsOverQuotaException extends IllegalStateException {
 
+	@Override
+	public String toString() {
+		return "Reached the maximum number of available transactions";
+	}
+	
 }
