9c1a0cbdfd44d6d8b544d0343fa582f03292be7c
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-11-14 20:08:15 +0100

    feat: support the implementation of Graph interface

--- src/it/unibo/oop/lab06/generics1/GraphImpl.java
+++ src/it/unibo/oop/lab06/generics1/GraphImpl.java
@@ -1,5 +1,38 @@
 package it.unibo.oop.lab06.generics1;
 
-public class GraphImpl {
+import java.util.List;
+import java.util.Set;
+
+public class GraphImpl<N> implements Graph<N> {
+
+	@Override
+	public void addNode(Object node) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addEdge(Object source, Object target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set nodeSet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set linkedNodes(Object node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getPath(Object source, Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
