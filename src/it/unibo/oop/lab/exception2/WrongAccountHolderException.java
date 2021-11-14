0f86a041da103d9fd198f0121fc38d4b385b32e7
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-11-14 16:56:10 +0100

    feat: add custom toString() method

--- src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
+++ src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
@@ -2,5 +2,10 @@ package it.unibo.oop.lab.exception2;
 
 @SuppressWarnings("serial")
 public class WrongAccountHolderException extends IllegalArgumentException {
+
+	@Override
+	public String toString() {
+		return "Unauthorized account";
+	}
 	
 }
