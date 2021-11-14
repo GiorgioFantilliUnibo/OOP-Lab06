1024b884949e37a531ada35b7584964594cd64fa
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-11-14 16:59:37 +0100

    fix: add no argument constructor to WrongAccountHolderException class

--- src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
+++ src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
@@ -2,6 +2,11 @@ package it.unibo.oop.lab.exception2;
 
 @SuppressWarnings("serial")
 public class WrongAccountHolderException extends IllegalArgumentException {
+	
+
+	public WrongAccountHolderException() {
+		super();
+	}
 
 	@Override
 	public String toString() {
