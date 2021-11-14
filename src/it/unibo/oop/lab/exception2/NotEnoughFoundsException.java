367a42c4e923127b8a5c9a1db503a731e9518134
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-11-14 17:33:00 +0100

    feat: add custom toString()

--- src/it/unibo/oop/lab/exception2/NotEnoughFoundsException.java
+++ src/it/unibo/oop/lab/exception2/NotEnoughFoundsException.java
@@ -2,5 +2,10 @@ package it.unibo.oop.lab.exception2;
 
 @SuppressWarnings("serial")
 public class NotEnoughFoundsException  extends IllegalStateException {
+	
+	@Override
+	public String toString() {
+		return "Not enought founds in the user's balance";
+	}
 
 }
