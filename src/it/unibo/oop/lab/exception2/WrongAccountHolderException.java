a561064829660ff7300f1bba6fea9ab23adb4062
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-11-14 17:35:41 +0100

    fix: remove useless costructor

--- src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
+++ src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
@@ -2,11 +2,6 @@ package it.unibo.oop.lab.exception2;
 
 @SuppressWarnings("serial")
 public class WrongAccountHolderException extends IllegalArgumentException {
-	
-
-	public WrongAccountHolderException() {
-		super();
-	}
 
 	@Override
 	public String toString() {
