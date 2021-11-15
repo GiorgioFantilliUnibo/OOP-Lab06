80878e12d8771b850e786c4b36d32d7a024528d9
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-11-15 14:49:16 +0100

    docs: inherit superclass javadoc

--- src/it/unibo/oop/lab06/generics1/GraphImpl.java
+++ src/it/unibo/oop/lab06/generics1/GraphImpl.java
@@ -5,30 +5,45 @@ import java.util.Set;
 
 public class GraphImpl<N> implements Graph<N> {
 
+	/**
+	 * {@inheritDocg}
+	 */
 	@Override
 	public void addNode(Object node) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * {@inheritDocg}
+	 */
 	@Override
 	public void addEdge(Object source, Object target) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * {@inheritDocg}
+	 */
 	@Override
 	public Set<N> nodeSet() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * {@inheritDocg}
+	 */
 	@Override
 	public Set<N> linkedNodes(Object node) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * {@inheritDocg}
+	 */
 	@Override
 	public List<N> getPath(Object source, Object target) {
 		// TODO Auto-generated method stub
